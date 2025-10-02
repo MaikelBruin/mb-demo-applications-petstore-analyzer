7c2fbfd97552b750e70e850ebe8c453497be0fcd
Maikel Bruin <maikel.bruin@randstadgroep.nl>
2025-10-02 07:36:16 +0200

    initial commit for spring boot starter project

---
+++ src/main/java/mb/demo/applications/petstore/analyzer/PetstoreAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package mb.demo.applications.petstore.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstoreAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mb/demo/applications/petstore/analyzer/PetstoreAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package mb.demo.applications.petstore.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
