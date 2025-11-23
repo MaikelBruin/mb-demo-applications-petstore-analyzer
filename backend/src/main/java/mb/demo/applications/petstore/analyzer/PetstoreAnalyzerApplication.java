4256f73ce06b44c7f4a8bbc68b1194cb8633dab4
Maikel Bruin <maikelbruin@gmail.com>
2025-11-23 16:41:47 +0100

    set name and description for swagger page

--- backend/src/main/java/mb/demo/applications/petstore/analyzer/PetstoreAnalyzerApplication.java
+++ backend/src/main/java/mb/demo/applications/petstore/analyzer/PetstoreAnalyzerApplication.java
@@ -1,8 +1,11 @@
 package mb.demo.applications.petstore.analyzer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info=@Info(title="Petstore Analyzer API", description = "API that analyzes data from the OpenAPI Petstore"))
 @SpringBootApplication
 public class PetstoreAnalyzerApplication {
 
